7803dda5acdc9d8ecae13cf4730d57b855e3f61f
Samuel Jabes <samueljcc@al.insper.edu.br>
2025-11-06 09:24:28 -0300

    feat: add sound shoot

--- Assets/Scripts/Sam/Shoot.cs
+++ Assets/Scripts/Sam/Shoot.cs
@@ -2,19 +2,37 @@ using UnityEngine;
 
 public class Shoot : MonoBehaviour
 {
+    [Header("Disparo")]
     public GameObject bullet;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float fireRate = 0.2f; // intervalo entre tiros (em segundos)
+
+    [Header("Áudio")]
+    [Range(0f, 1f)] public float volume = 1f;
+    [Range(0f, 0.5f)] public float pitchVariance = 0.05f;
+
+    private AudioSource audioSource;
+    private float nextFireTime = 0f;
+
+    void Awake()
     {
-        
+        audioSource = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(0) && Time.time >= nextFireTime)
         {
+            nextFireTime = Time.time + fireRate;
+
             Instantiate(bullet, transform.position, transform.rotation);
-        }   
+
+            if (audioSource != null && audioSource.clip != null)
+            {
+                float oldPitch = audioSource.pitch;
+                audioSource.pitch = 1f + Random.Range(-pitchVariance, pitchVariance);
+                audioSource.PlayOneShot(audioSource.clip, volume);
+                audioSource.pitch = oldPitch;
+            }
+        }
     }
 }
